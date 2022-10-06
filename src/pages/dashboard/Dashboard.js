085aeecb0f4e65e35505893ce0e66759229fb94d
George <nithyax.george@intel.com>
2022-10-06 17:28:03 +0530

    highlight text changes

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -26,12 +26,6 @@ function Dashboard(props) {
     useEffect(() => {
         props.getCognitiveData();
     }, [])
-    // const layout = [
-    //     { i: "a", x: 0, y: 0, w: 8, h: 20 },
-    //     { i: "b", x: 1, y: 0, w: 3, h: 2},// minW: 2, maxW: 4 },
-    //     { i: "c", x: 4, y: 0, w: 1, h: 2 }
-    //   ];
-    // const layout = Layout && Layout["md"];
     const ResponsiveGridLayout = WidthProvider(Responsive);
     let data = props.trace//[props.trace1, props.trace2]
     var graphLayout = {
@@ -75,14 +69,6 @@ function Dashboard(props) {
     const onHideDetail = (name) => {
         setDisplayDialogDetail(false);
     }
-    // const renderFooter = (name) => {
-    //     return (
-    //         <div>
-    //             <Button label="No" icon="pi pi-times" onClick={() => onHide(name)} className="p-button-text" />
-    //             <Button label="Yes" icon="pi pi-check" onClick={() => onHide(name)} autoFocus />
-    //         </div>
-    //     );
-    // }
     const jobColumns = [
         {field: 'company', header: 'Company'},
         {field: 'date', header: 'Date'},
@@ -115,38 +101,12 @@ function Dashboard(props) {
     const dynamicTweetsColumns = tweetsColumns.map((col,i) => {
         return <Column key={col.field} field={col.field} header={col.header} />;
     });
-    // const products = [
-    //     {"id": "1000","code": "f230fh0g3","name": "Bamboo Watch","description": "Product Description","image": "bamboo-watch.jpg","price": 65,"category": "Accessories","quantity": 24,"inventoryStatus": "INSTOCK","rating": 5},
-    //     {"id": "1001","code": "nvklal433","name": "Black Watch","description": "Product Description","image": "black-watch.jpg","price": 72,"category": "Accessories","quantity": 61,"inventoryStatus": "INSTOCK","rating": 4},
-    //     {"id": "1002","code": "zz21cz3c1","name": "Blue Band","description": "Product Description","image": "blue-band.jpg","price": 79,"category": "Fitness","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 3},
-    //     {"id": "1003","code": "244wgerg2","name": "Blue T-Shirt","description": "Product Description","image": "blue-t-shirt.jpg","price": 29,"category": "Clothing","quantity": 25,"inventoryStatus": "INSTOCK","rating": 5},
-    //     {"id": "1004","code": "h456wer53","name": "Bracelet","description": "Product Description","image": "bracelet.jpg","price": 15,"category": "Accessories","quantity": 73,"inventoryStatus": "INSTOCK","rating": 4},
-    //     {"id": "1005","code": "av2231fwg","name": "Brown Purse","description": "Product Description","image": "brown-purse.jpg","price": 120,"category": "Accessories","quantity": 0,"inventoryStatus": "OUTOFSTOCK","rating": 4},
-    //     {"id": "1006","code": "bib36pfvm","name": "Chakra Bracelet","description": "Product Description","image": "chakra-bracelet.jpg","price": 32,"category": "Accessories","quantity": 5,"inventoryStatus": "LOWSTOCK","rating": 3},
-    //     {"id": "1007","code": "mbvjkgip5","name": "Galaxy Earrings","description": "Product Description","image": "galaxy-earrings.jpg","price": 34,"category": "Accessories","quantity": 23,"inventoryStatus": "INSTOCK","rating": 5},
-    //     {"id": "1008","code": "vbb124btr","name": "Game Controller","description": "Product Description","image": "game-controller.jpg","price": 99,"category": "Electronics","quantity": 2,"inventoryStatus": "LOWSTOCK","rating": 4},
-    //     {"id": "1009","code": "cm230f032","name": "Gaming Set","description": "Product Description","image": "gaming-set.jpg","price": 299,"category": "Electronics","quantity": 63,"inventoryStatus": "INSTOCK","rating": 3}
-    // ]
 
-    // const getHighlightedText1 = (text, highlight) => {
-    //     // Split on highlight term and include term into parts, ignore case
-    //     const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
-    //     let a = 0;
-    //     return <span> { parts.map((part, i) => 
-    //         // {if(part.toLowerCase() === highlight.toLowerCase()) {
-    //             // a=1;
-    //             <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { background: "#FFCC99" } : {} }>
-    //                 { part }
-    //             </span>
-    //         // }}
-    //         )
-    //     } </span>;
-    // }
     // const getHighlightedText = (text) => {
     //     let selectCompanyTemp = priValues2(props.getGraphClickCompany);
     //     let selectedCompany = selectCompanyTemp && selectCompanyTemp.split("|");
-    //     let option1 = "alinks|ansys|ansys\ hpc|ansys\ rf|designer\ rf|designmodeler|designspace|designxplorer|exalto|fluent|geometry\ interface|hfss|human\ body\ model|icepack|maxwell|mechanical|multiphysics|optimetrics|path\ fx|pathfinder|power\ artist|q3d|raptorx|redhawk|redhawk\ sc|seahawk|sentinel|siwave|sow|spaceclaim|totem|variance\ fx|velocerf"
-    //     let option2 = "assertion\ based\ verification|avm|cdc|clock\ domain\ crossing|directed\ random\ verification|fev|formal\ equivalence\ verification|formal\ property\ verification|fpv|functional\ coverage|functional\ verification|hardware\ description\ language|hdl|ip\-xact|logic\ equivalence\ checking|logic\ simulation|metric\ driven\ verification|open\ verification\ library|ovm|portable\ stimulus|pss|random\ verification|rdc|register\ transfer\ level|reset\ domain\ crossing|rtl|rtl\ coverage\ |rtl\ debug|rtl\ design\ entry|rtl\ development|rtl\ lint|rtl\ modeling|rtl\ simulation|rtl\ simulator|rtl\ simulator\ performance|rtl\ validation|rtl\ verification|silicon\ verification\ ip|simulation\ performance|simulation\ regression|static\ verification|sva|systemrdl|systemverilog|systemverilog\ assertions|test\ bench|test\ suite|testbench|universal\ verification\ methodology|uvm|verification\ environment|verilog|vhdl|vmm"
+    //     let option1 = "alinks|ansys|ansys hpc|ansys rf|designer rf|designmodeler|designspace|designxplorer|exalto|fluent|geometry interface|hfss|human body model|icepack|maxwell|mechanical|multiphysics|optimetrics|path fx|pathfinder|power artist|q3d|raptorx|redhawk|redhawk sc|seahawk|sentinel|siwave|sow|spaceclaim|totem|variance fx|velocerf"
+    //     let option2 = "assertion based verification|avm|cdc|clock domain crossing|directed random verification|fev|formal equivalence verification|formal property verification|fpv|functional coverage|functional verification|hardware description language|hdl|ip-xact|logic equivalence checking|logic simulation|metric driven verification|open verification library|ovm|portable stimulus|pss|random verification|rdc|register transfer level|reset domain crossing|rtl|rtl coverage |rtl debug|rtl design entry|rtl development|rtl lint|rtl modeling|rtl simulation|rtl simulator|rtl simulator performance|rtl validation|rtl verification|silicon verification ip|simulation performance|simulation regression|static verification|sva|systemrdl|systemverilog|systemverilog assertions|test bench|test suite|testbench|universal verification methodology|uvm|verification environment|verilog|vhdl|vmm"
     //     let count = 1;
     //     if(text && text.length) {
     //         // let resp = getHighlightedText1(text,selectedCompany[2]);
@@ -193,9 +153,25 @@ function Dashboard(props) {
     //         } </span>;
     //     })
     // }
+    let options1 = "alinks|ansys|ansys hpc|ansys rf|designer rf|designmodeler|designspace|designxplorer|exalto|fluent|geometry interface|hfss|human body model|icepack|maxwell|mechanical|multiphysics|optimetrics|path fx|pathfinder|power artist|q3d|raptorx|redhawk|redhawk sc|seahawk|sentinel|siwave|sow|spaceclaim|totem|variance fx|velocerf"
+    let options2 = "assertion based verification|avm|cdc|clock domain crossing|directed random verification|fev|formal equivalence verification|formal property verification|fpv|functional coverage|functional verification|hardware description language|hdl|ip-xact|logic equivalence checking|logic simulation|metric driven verification|open verification library|ovm|portable stimulus|pss|random verification|rdc|register transfer level|reset domain crossing|rtl|rtl coverage |rtl debug|rtl design entry|rtl development|rtl lint|rtl modeling|rtl simulation|rtl simulator|rtl simulator performance|rtl validation|rtl verification|silicon verification ip|simulation performance|simulation regression|static verification|sva|systemrdl|systemverilog|systemverilog assertions|test bench|test suite|testbench|universal verification methodology|uvm|verification environment|verilog|vhdl|vmm"
     const getSelectionList = () => {
+        let selectedCompany = [];
         let selectCompanyTemp = priValues2(props.getGraphClickCompany);
-        let selectedCompany = selectCompanyTemp && selectCompanyTemp.split("|");
+        if(selectCompanyTemp && selectCompanyTemp.length) {
+            selectedCompany = selectCompanyTemp.split("|");
+        }
+        let option1 = "alinks|ansys|ansys hpc|ansys rf|designer rf|designmodeler|designspace|designxplorer|exalto|fluent|geometry interface|hfss|human body model|icepack|maxwell|mechanical|multiphysics|optimetrics|path fx|pathfinder|power artist|q3d|raptorx|redhawk|redhawk sc|seahawk|sentinel|siwave|sow|spaceclaim|totem|variance fx|velocerf"
+        let option2 = "assertion based verification|avm|cdc|clock domain crossing|directed random verification|fev|formal equivalence verification|formal property verification|fpv|functional coverage|functional verification|hardware description language|hdl|ip-xact|logic equivalence checking|logic simulation|metric driven verification|open verification library|ovm|portable stimulus|pss|random verification|rdc|register transfer level|reset domain crossing|rtl|rtl coverage |rtl debug|rtl design entry|rtl development|rtl lint|rtl modeling|rtl simulation|rtl simulator|rtl simulator performance|rtl validation|rtl verification|silicon verification ip|simulation performance|simulation regression|static verification|sva|systemrdl|systemverilog|systemverilog assertions|test bench|test suite|testbench|universal verification methodology|uvm|verification environment|verilog|vhdl|vmm"
+        
+        let opt1 = option1 && option1.split("|");
+        let opt2 = option2 && option2.split("|");
+        opt1.map((val) => {
+                return selectedCompany.push(val);
+        })
+        opt2.map((val) => {
+                return selectedCompany.push(val)
+        })
         return selectedCompany
     }
     function Highlight({ children: text = "", tags = [] }) {
@@ -208,12 +184,13 @@ function Dashboard(props) {
             {matches.map((match, i) => {
               const startIndex = match.index;
               const currentText = match[0];
+              console.log("current text",currentText)
               const endIndex = startIndex + currentText.length;
               const nextIndex = matches[i + 1]?.index;
               const untilNextText = text.slice(endIndex, nextIndex);
               return (
                 <span key={i}>
-                  <mark style={{ background: "#FFCC99" }}>{currentText}</mark>
+                  <mark style={options1.includes(currentText.toLowerCase()) ? { background: "#f7f28f" } : options2.includes(currentText.toLowerCase()) ? {"background":"#a0edf2"} : { background: "#FFCC99" }}>{currentText}</mark>
                   {untilNextText}
                 </span>
               );
@@ -222,46 +199,6 @@ function Dashboard(props) {
         );
       }
 
-    const processedTextContent = (val) => {
-        let selectCompanyTemp = priValues2(props.getGraphClickCompany);// priValues2(selectedDetail.company);
-        let selectedCompany = selectCompanyTemp && selectCompanyTemp.split("|");
-        let str = "";
-
-        // if(selectedCompany && selectedCompany.length) {
-        //     for(let i=0;i<selectedCompany.length;i++) {
-        //         if(val && val.includes(" ")) {
-        //             let value = val.split(" ");
-        //             for(let j=0;j<value.length;j++) {
-        //                 let count = 1;
-        //                 if(value[j].includes(selectedCompany[i].toLowerCase())) {
-        //                     count = 0;
-        //                     if(str.length == 0) {
-        //                         str += value[j]
-        //                     }
-        //                     else {
-        //                     str += " " + "<span style={{'background': 'yellow'}}>{value[j]}</span>" + " ";
-        //                     }
-        //                 }
-        //                 if(count) {
-        //                     if(str.length == 0) {
-        //                         str += value[j]
-        //                     }
-        //                     else {
-        //                         str = str + " " + value[j] + " "
-        //                     }
-        //                 }
-        //             }
-        //         } else {
-        //             if(val && val.includes(selectedCompany[i].toLowerCase())) {
-        //                 str = " " + "<span style={{'background': 'yellow'}}>{val}</span>" + " ";
-        //             } else {
-        //                 str = " " + val + " "
-        //             }
-        //         }
-        //     }
-        // }
-        return (str)
-    }
     return (
         <div>
             {/* <ResponsiveGridLayout
@@ -284,7 +221,6 @@ function Dashboard(props) {
                 <div key="graph3" style={{ 'border': '1px solid' }}>c</div> */}
             {/* </ResponsiveGridLayout> */}
             <Dialog visible={displayBasic} style={{ width: '50vw' }} onHide={() => onHide('displayBasic')}>
-                {/* <p>Hello {label1} and {value1}</p> */}
                 <p style={{'color': 'blue'}}>Input "all" under Company filter to view all companies</p>
                 <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
                     <TabPanel header="Jobs">
@@ -302,14 +238,6 @@ function Dashboard(props) {
                 </TabView>
             </Dialog>
             <Dialog header="Selected Data" visible={displayDialogDetail} style={{ width: '50vw' }} onHide={() => onHideDetail('displayDialogDetail')}>
-                {/* <p>jobkey : {processedTextContent(selectedDetail.jobKey)}</p>
-                <p>jobtitle : {processedTextContent(selectedDetail.jobtitle)}</p>
-                <p>company : {processedTextContent(selectedDetail.company)}</p>
-                <p>city : {processedTextContent(selectedDetail.city)}</p>
-                <p>state: {processedTextContent(selectedDetail.state)}</p>
-                <p>date_posted: {processedTextContent(selectedDetail.date_posted)}</p>
-                <p>create_date: {processedTextContent(selectedDetail.create_date)}</p>
-                <p>text_content: {processedTextContent(selectedDetail.text_content)}</p> */}
                 <p>jobkey : 
                     <Highlight tags={getSelectionList()}>
                         {selectedDetail.jobKey}
@@ -370,8 +298,6 @@ const mapStateToProps = (state) => {
         modalNewsData: state.dashboard.modalNewsData,
         modalTweetsData: state.dashboard.modalTweetsData,
         getGraphClickCompany: state.dashboard.getGraphClickCompany
-        // trace1: state.dashboard.trace1,
-        // trace2: state.dashboard.trace2
     }
 }
 
